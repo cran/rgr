e8b19f482db28f4e20afa619fc7f4c2ecbe57559
Robert G. Garrett <garrett@NRCan.gc.ca>
2013-03-18 19:30:48 +0000

    version 1.1.9

--- R/df.test.R
+++ R/df.test.R
@@ -1,24 +1,22 @@
 df.test <-
-function (dfname, x = NULL) 
+function(dfname)
 {
-    dfn <- deparse(substitute(dfname))
-    if (exists(dfn)) {
+     # Function to check for the existence of a data frame and whether it
+     # is already attached.  If the data frame is 'legitimate' the variable
+     # names are displayed with its dimensions.
+     #
+     dfn <- deparse(substitute(dfname))
+     if (exists(dfn)) {
         if (match(dfn, search(), nomatch = 0)) 
             cat(" ", dfn, "is already attached\n")
         else {
-            attach(dfname)
-            on.exit(detach(dfname))
+            data(dfn)
+            cat(" ", dfn, "is not attached\n")
         }
         cat(paste("  Names of variables in ", dfn, ":\n ", sep = ""), 
             names(dfname), "\n")
-        if (!is.null(x)) {
-            xname <- deparse(substitute(x))
-            if (match(xname, names(dfname), nomatch = 0)) 
-                cat(paste("  Length of ", xname, ":", sep = ""), 
-                  length(x), "\n")
-            else cat(" ", xname, "is not present in", dfn, "\n")
-        }
+        cat(paste("  Dimensions of ", dfn, ": ", sep = ""), dim(dfname), "\n")
     }
-    else cat(" ", dfn, "is not in the search path\n")
+    else cat(" ", dfn, "is not in the workspace\n")
     invisible()
 }
